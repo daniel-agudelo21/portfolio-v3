d72f919b081af2da61c676166ce6a510461d2add
Daniel Agudelo <daniel.agudelo185@gmail.com>
2025-05-19 22:39:27 -0500

    i18n implementation

---
+++ src/i18n/ui.ts
@@ -0,0 +1,124 @@
+export const languages = {
+    es: 'Español',
+    en: 'English'
+}
+
+export const defaultLanguage = 'en'
+export const showDefaultLang = false;
+
+export const ui = {
+    en: {
+        "nav.education": 'Education',
+        "nav.experience": 'Experience',
+        "nav.projects": 'Projects',
+        "nav.skills": 'Skills',
+        "nav.about": 'About me',
+        "nav.contact": 'Contact',
+        "home.summary": "I’m a Full Stack Web Developer focused on Frontend, here to bring ideas to life and create digital experiences that make an impact.",
+        "home.contactme": "Contact me",
+        "home.cv": "Resume",
+        "education.certificate": "Certificates in",
+        "education.title": "Education",
+        "education.subtitle": "Education in web development using HTML5, CSS3, JavaScript, ReactJs, NextJs, Astro, JSON, Node.js, TailwindCSS",
+        "education.itemDate1": "June 2023 - October 2023",
+        "education.itemTitle1": "Full Stack Developer",
+        "education.itemDate2": "March 2022 - December 2022",
+        "education.itemTitle2": "Diploma with Emphasis in Web Development",
+        "education.itemDate3": "November 2020 - February 2022",
+        "education.itemTitle3": "Software Development Technician",
+        "experience.work": "Work",
+        "experience.subtitle": "Experience in web development using HTML5, CSS3, JavaScript, ReactJs, NextJs, Astro, JSON, Node.js, TailwindCSS",
+        "experience.date1": "March 2025 - Present",
+        "experience.title1": "Full Stack Developer",
+        "experience.company1": "BluRealty",
+        "experience.description1": "I actively contribute to the development of static and dynamic components, data integration through APIs, and seamless connection between backend services and the user interface. Additionally, I participate in architectural decisions, apply development best practices, and conduct code reviews. I work autonomously and collaborate with cross-functional teams to ensure scalable, maintainable solutions aligned with product goals.",
+        "experience.date2": "September 2023 - January 2024",
+        "experience.title2": "Junior Analyst Developer",
+        "experience.company2": "Talento Cloud",
+        "experience.description2": "I collaborated on key projects to internationalize the platform and enhance reporting tools, focusing on optimizing global accessibility and providing efficient tools for data analysis.",
+        "experience.date3": "August 2023 - October 2023",
+        "experience.description3": "I facilitated a dynamic and collaborative learning environment for Soy Henry students, co-coordinating daily sessions and guiding them in exploring and understanding key technologies such as React, Express, Node.js, Sequelize, and PostgreSQL.",
+        // projects
+        "projects.description1": "Application where you can view information about Valorant agents, Riot Games’ tactical shooter, with filters by role and name, an agent detail section, and smooth navigation. Fully responsive for different screen sizes.",
+        "projects.description2": "This e-commerce platform was designed to offer a unique virtual space for street vendors, allowing them to create their own online stores and sell handcrafted products easily and securely. My contribution focused on implementing an intuitive and visually appealing user interface.",
+        "projects.description3": "Frontend practice project. I used React for the interface, TailwindCSS for layout and styling, and RapidAPI to access essential YouTube data, providing an authentic user experience.",
+        "projects.description4": "In this project, I focused on developing the frontend of an inventory manager for a hardware store, using modern technologies like React, Next.js, and Redux. The goal was to create an intuitive and efficient experience for managing products, stock, pricing, and inventory movements.",
+        "projects.destacated": "Featured",
+        "projects.collections": "A collection of projects I’ve recently worked on",
+        // skills
+        "skills.title": "Skills",
+        "skills.learning": "Currently learning...",
+        // about me
+        "about.hi": "Hi, I'm",
+        "about.text1": `After finishing high school, my academic path was uncertain. I initially pursued engineering on recommendation, but social pressure led me to reconsider. After exploring different options, <strong>programming crossed my path again—this time with unshakable determination.</strong>`,
+        "about.text2": "The Soy Henry Bootcamp marked a turning point. In addition to technical foundations, <strong>I was recognized for my teamwork skills, earning a medal.</strong>",
+        "about.text3": "<strong>My main interest lies in Frontend development, web design, and user experience</strong>, as I explore the fascinating world of UI/UX. I firmly believe that blending technical functionality with aesthetics is key to creating unique experiences.",
+        "about.quote": "The road to excellence is always under construction.",
+        "about.quoteAuthor": "Marcus Aurelius",
+        // contact
+        "contact.title": "Let's Connect",
+        "contact.subtitle": "Let's make magic together",
+        "contact.click":"Click below to communicate",
+        // Footer
+        "footer.top":"Top"
+
+
+    },
+    es: {
+        "nav.education": 'Educación',
+        "nav.experience": 'Experiencia',
+        "nav.projects": 'Proyectos',
+        "nav.skills": 'Habilidades',
+        "nav.about": 'Sobre mi',
+        "nav.contact": 'Contacto',
+        "home.summary": "Soy un desarrollador web Full Stack enfocado en el FrontEnd,estoy aquí para dar vida a ideas y crear experiencias digitales que dejen huella.",
+        "home.contactme": "Contáctame",
+        "home.cv": "Hoja de vida",
+        "education.certificate": "Certificados de",
+        "education.title": "Educación",
+        "education.subtitle": "Educación en desarrollo de páginas web usando HTML5, CSS3, JavaScript, ReactJs, NextJs, Astro, JSON, Node.js, TailwindCSS",
+        "education.itemDate1": "Junio 2023 - Octubre 2023",
+        "education.itemTitle1": "Desarrollador Full Stack",
+        "education.itemDate2": "Marzo 2022 - Diciembre 2022",
+        "education.itemTitle2": "Diplomado con énfasis desarrollo web",
+        "education.itemDate3": "Noviembre 2020 - Febrero 2022",
+        "education.itemTitle3": "Técnico en desarrollo de software",
+        // experience section
+        "experience.work": "Laboral",
+        "experience.subtitle": "Experiencia en desarrollo de páginas web usando HTML5, CSS3, JavaScript, ReactJs, NextJs, Astro, JSON, Node.js, TailwindCSS",
+        // experience data
+        "experience.date1": "Marzo 2025 - Actualidad",
+        "experience.title1": "Desarrollador Full Stack",
+        "experience.company1": "BluRealty",
+        "experience.description1": "Participo activamente en el desarrollo de componentes estáticos y dinámicos, integración de datos mediante APIs, y conexión eficiente entre servicios backend y la interfaz de usuario. Además, colaboro en decisiones arquitectónicas, aplico buenas prácticas de desarrollo y realizo revisiones de código. Trabajo de forma autónoma y en coordinación con otros equipos para asegurar soluciones escalables, mantenibles y alineadas con los objetivos del producto.",
+        "experience.date2": "Septiembre 2023 - Enero 2024",
+        "experience.title2": "Desarrollador Analista Junior",
+        "experience.company2": "Talento Cloud",
+        "experience.description2": "Colaboré en proyectos clave para internacionalizar la plataforma y mejorar informes, enfocándome en optimizar la accesibilidad global y brindar herramientas eficientes para el análisis de datos.",
+        "experience.date3": "Agosto 2023 - Octubre 2023",
+        "experience.description3": "Facilité un entorno de aprendizaje dinámico y colaborativo para alumnos de Soy Henry, co-coordinando un espacio diario y guiándolos en la exploración y comprensión de tecnologías clave como React, Express, Node.js, Sequelize y PostgreSQL.",
+        // Projects
+        "projects.description1": "Aplicacion donde puedes visualizar la informacion de los agentes de Valorant, Tactical Shooter de Riot Games, con filtros por roles y nombre, seccion de detalle del agente y navegacion fluida. Totalmente responsive para diferentes formatos",
+        "projects.description2": "Este e-commerce fue diseñado con el propósito de ofrecer una plataforma virtual única para vendedores ambulantes, permitiéndoles crear sus propias tiendas online y vender productos artesanales de manera fácil y segura. Mi contribución se centró en la implementación de una interfaz de usuario intuitiva y atractiva.",
+        "projects.description3": "Práctica de FrontEnd. Utilicé React para la interfaz, TailwindCSS para el diseño y estilos, junto con RapidApi para acceder a datos esenciales de YouTube, ofreciendo una experiencia de usuario auténtica.",
+        "projects.description4": "En este proyecto, me enfoqué en el desarrollo del front-end de un gestor de inventario para una ferretería, utilizando tecnologías modernas como React, Next.js y Redux. El objetivo era crear una experiencia intuitiva y eficiente para la gestión de productos, stock, precios y movimientos.",
+        "projects.destacated": "Destacados",
+        "projects.collections": "Una colección de los proyectos que he realizado recientemente",
+        // skills
+        "skills.title": "Destrezas o habilidades",
+        "skills.learning": "Aprendiendo...",
+        // about me
+        "about.hi": "Hola, soy",
+        "about.text1": "Al salir del colegio, mi elección académica fue un camino indeciso. Inicialmente en ingeniería por recomendación, pero la presión social me llevó a reconsiderar. Después de explorar opciones, <strong>la programación se cruzó de nuevo en mi camino, esta vez con una determinación inquebrantable.</strong>",
+        "about.text2": "El Bootcamp Soy Henry marcó un punto de inflexión. Además de las bases técnicas, <strong>fui reconocido por mi habilidad en el trabajo en equipo,obteniendo una medalla.</strong >",
+        "about.text3": "<strong>Mi interés se inclina hacia el FrontEnd, el diseño web y laexperiencia del usuario</strong>, explorando el fascinante mundo del UI/UX. Creo firmemente que la fusión de funcionalidad técnica y estética es clave para experiencias únicas.",
+        "about.quote": "El camino hacia la excelencia está siempre en construcción.",
+        "about.quoteAuthor": "Marco Aurelio",
+        // contact
+        "contact.title": "Conectémonos",
+        "contact.subtitle": "Hagamos magia juntos",
+        "contact.click":"Haga clic abajo para comunicarse",
+        // Footer
+        "footer.top":"Inicio"
+    }
+} as const
